c8d98eb0988201809fb68b6a97a5b43a95301da0
Gabriel Gomes <61382317+all0yy@users.noreply.github.com>
2021-04-29 20:26:33 -0300

    Update correios_chan.py
    -correção de código
    -automação de envio da mensagem

--- correios_chan.py
+++ correios_chan.py
@@ -9,12 +9,7 @@ from discord.ext import commands
 
 
 
-prefix = "$"
-bot = commands.Bot(command_prefix=prefix)
-
-
 client = discord.Client()
-
 
 
 @client.event
@@ -23,31 +18,40 @@ async def on_ready():
     activity = discord.Game(name="Temos nosso própio tempo >.<", type=3)
     await client.change_presence(status=discord.Status.idle, activity=activity)
 
+@client.event
+async def get_emote(emote,texto):
+    if "saiu" in texto:
+        emote = ":white_check_mark:"
+    elif "objeto" in texto:
+        emote = ":ballot_box_with_check:"
+
 
 
 @client.event
 async def get_status(codigo):
         try:
             global resposta
+            global texto
             req = requests.post(url="https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm?", data={"objetos" : codigo})
             soup = BeautifulSoup(req.text,'html.parser')
             texto = soup.find(id='UltimoEvento').strong.text
             data = soup.find(id='UltimoEvento').text.split()[-1]
-            resposta = f' :ballot_box_with_check:  **{texto}** - Data - **{data}** Código n°: **{codigo}**'
+            resposta = f' **{texto}** | Data: **{data}** | Código n°: **{codigo}**'
             print(resposta) 
-            channel = client.get_channel(ID DO CANAL)
-            await channel.send(resposta)
         except Exception as e:
-            channel = client.get_channel(ID DO CANAL)
             await channel.send(" :x: **Pacote não encontrado, certifique de que o código esteja correto. (Compras internacionais podem demorar para serem registradas).**")
 
-
-
-
+@client.event
+async def send_status(texto):
+    if "entrega" in texto:
+        await channel.send(":white_check_mark:" + resposta)
+    elif "Objeto" in texto:
+        await channel.send(":ballot_box_with_check:" + resposta)
 
 
 @client.event
 async def on_message(message):
+    global channel
     if message.author == client.user:
         return
     if message.content.startswith('!c'):
@@ -56,13 +60,6 @@ async def on_message(message):
     if message.content.startswith('!c'):
         codigo = message.content.replace("!c ", "")
         await get_status(codigo)
+        await send_status(texto)
 
-
-
-
-
-
-
-
-
-client.run('APIKEY')
+client.run('DISCORD API KEY AQUI')
